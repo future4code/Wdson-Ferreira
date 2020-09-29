f491d197a589623ba707dd4d738305773f25c9c6
wdsondallas <wdson_henrique@hotmail.com>
2020-09-29 00:09:58 -0300

    alteracao index.js

--- semana3/variaveis/index.js
+++ semana3/variaveis/index.js
@@ -1,3 +1,4 @@
+/*
 let name;
 let age;
 
@@ -11,6 +12,8 @@ console.log(typeof name); // Tipo string.
 console.log(typeof age); // O prompt mesmo usando um valor do tipo number ele imprime string.
 
 console.log('Olá ' + name + ' você tem ' + age + ' anos.');
+*/
+
 
 let myColor = prompt('Qual a sua cor preferida? ');
 let myAdress = prompt('Qual é o seu endereço? ');
@@ -23,3 +26,17 @@ console.log('Qual é o seu endereço: ' + myAdress);
 console.log('Qual é o seu filme preferido: ' + myMovie);
 console.log('Qual é a sua música favorita: ' + myMusic);
 console.log('Qual é o seu livro favorito: ' + myBook);
+
+let myFood = ['Pizza', 'Hot dog', 'Burguer', 'Spaghetti', 'Lasanha'];
+console.log(myFood);
+
+console.log('Essas as minhas comidas preferidas: ');
+console.log(myFood[0]);
+console.log(myFood[1]);
+console.log(myFood[2]);
+console.log(myFood[3]);
+console.log(myFood[4]);
+
+foodUser = prompt('Qual a sua comida preferida : ');
+myFood[1] = foodUser;
+console.log(myFood);
