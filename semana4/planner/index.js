cffc143757ea58f5ee2afe1d6f43504cb7f13a01
wdsondallas <wdson_henrique@hotmail.com>
2020-10-06 17:43:52 -0300

    atualizacao

---
+++ semana4/planner/index.js
@@ -0,0 +1,15 @@
+function adicionaTarefa() {
+	const minhaTarefa = document.getElementById("tarefa")
+
+	const diasDaSemana = document.getElementById("dias-semana").value
+	const minhaLista= document.getElementById(diasDaSemana)
+
+	if (minhaTarefa !== "") {
+
+		minhaLista.innerHTML += `<li> ${minhaTarefa.value} </li>`
+		
+	}
+
+	minhaTarefa.value = ""
+	console.log(diasDaSemana)
+}
