b6fc6a7ec60bfd51e11b0aacbf0a24a6bd351810
wdsondallas <wdson_henrique@hotmail.com>
2020-10-09 15:00:10 -0300

    questao 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,7 @@
+/*
+1 -  
+	Uma funcao que converte Reais em Dolar.
+	A funcao recebe um valor digitado pelo usuario e multiplica com o valor do parametro passado na invocacao da funcao.
+	Imprime no console o resultado dessa multiplicacao 600.
+
+*/ 
