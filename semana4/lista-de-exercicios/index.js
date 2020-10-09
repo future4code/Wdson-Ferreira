1bc2682126eb707fca47434517a9ac4f18e65d80
wdsondallas <wdson_henrique@hotmail.com>
2020-10-09 15:58:28 -0300

    questao 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -2,6 +2,14 @@
 1 -  
 	Uma funcao que converte Reais em Dolar.
 	A funcao recebe um valor digitado pelo usuario e multiplica com o valor do parametro passado na invocacao da funcao.
-	Imprime no console o resultado dessa multiplicacao 600.
+    Imprime no console o resultado dessa multiplicacao 600.
+    
+2 -
+	Um funcao para tipos de investimentos.
+	A funcao tem um switch case com varios tipos de investimentos sendo: tipo, valor e quanto rende.
+	Existe uma variavel que retorna o valor *  rendimento.
+	Fora do escopo da funcao temos duas variaveis que recebem a invocacao da funcao com tipo e o valor.
+	Uma varivel imprime no console 165, pois o tipo escolhido foi "Acoes" com valor 150 * rendimento 1.1
+	A outra variavel imprime, "TIPO DE INVESTIMENTO INFORMADO INCORRETO! ", pois o default do switch case existe um alert com esta mensagem.
 
 */ 
