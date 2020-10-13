1659a7c06aa696129cd1bf89d3ca4556940fe9af
Wdson Henrique <31639036+wdsonhenrique@users.noreply.github.com>
2020-10-12 23:00:21 -0300

    Update index.js

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,8 +1,5 @@
 /*
-1 -  
-	Uma funcao que converte Reais em Dolar.
-	A funcao recebe um valor digitado pelo usuario e multiplica com o valor do parametro passado na invocacao da funcao.
-    Imprime no console o resultado dessa multiplicacao 600.
+
     
 2 -
 	Um funcao para tipos de investimentos.
@@ -11,12 +8,6 @@
 	Fora do escopo da funcao temos duas variaveis que recebem a invocacao da funcao com tipo e o valor.
 	Uma varivel imprime no console 165, pois o tipo escolhido foi "Acoes" com valor 150 * rendimento 1.1
     A outra variavel imprime, "TIPO DE INVESTIMENTO INFORMADO INCORRETO! ", pois o default do switch case existe um alert com esta mensagem.
-
-3 - 
-	Dado um array numeros e outros dois arrays vazios array1 e array2
-	dentro de um laço de repetição for of temos uma condição para que todos os números pares do array numeros
-	seja colocado no array1 e todos números ímpares sejam colocados no array2.
-	Por fim ele imprime no console tamanho do array numeros que é 14,  array1 é 6  e array2 é 8.
 
 
 */ 
