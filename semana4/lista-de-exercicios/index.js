ffa7ae582428e6a7a78d2bffe1a44c0a1b7e2278
wdsondallas <wdson_henrique@hotmail.com>
2020-10-13 00:19:36 -0300

    questao 4

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,9 +1,9 @@
-/*
 
+/*
 4- 
 	Verifica o menor e maior numero do array usando o Infinity
 	pecorre o array usando for of depois criamos 2 condicionais um para mostrar 
 	o menor numnero e outra para mostrar o maior numero
 	no fim ele imprime no console -10 e 1590
 
-*/ 
+*/
